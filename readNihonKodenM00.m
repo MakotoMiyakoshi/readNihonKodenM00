46e8ac06a1c2b6b8ff036930fce23fc4ac762a43
Makoto Miyakoshi <mmiyakoshi@ucsd.edu>
2024-10-27 11:38:07 -0400

    Updated. Complete renewal.

--- readNihonKodenM00.m
+++ readNihonKodenM00.m
@@ -1,20 +1,22 @@
 % readNihonKodenM00 - reads Nihon Koden .m00 text files.
 %
-% Usage  : [output,srate,scale] = readNihonKodenM00(input);
+% Usage  : [output,srate,scale,chanlabels] = readNihonKodenM00(input);
 %
-% Inputs : path to the Nihon Koden .m00 text file
+% Inputs : Fullpath to the Nihon Koden .m00 text file
 % 
 % Outputs: Matlab variable in channels x timepoints, sampling rate, and
-%          amplitude scale. If 'scale' is 1, no need to rescale the data. If
-%          'scale'~=1, rescale the data by the factor of 'scale'. 
+%          amplitude scale in bins/microV (usually 1, which means the unit
+%          of the is in microV). If 'scale' is 1, no need to rescale the data. If
+%          'scale'~=1, you usually need to rescale the data by the factor of 'scale'. 
 %
 % History:
+% 10/27/2024 Makoto. Error report by Atsuhito. A new type of data format found. Complete renewal.
 % 11/26/2019 Makoto. Data provided by Elham Sherkat, which has 'Trigger' in the last channel, is supported.
 % 09/10/2018 ver 1.02 by Brian Silverstein. Added parsing of channel labels in second line.
 % 02/01/2017 ver 1.01 by Makoto. C = strsplit(firstLine, ' '); has raw vector.
 % 07/17/2013 ver 1.00 by Makoto. Created for my collaboration with Eishi Asano.
 
-% Copyright (C) 2013 Makoto Miyakoshi SCCN,INC,UCSD
+% Copyright (C) 2013 Makoto Miyakoshi SCCN,INC,UCSD. Cincinnati Children's Hospital Medical Center.
 %
 % 1. Redistributions of source code must retain the above copyright notice,
 % this list of conditions and the following disclaimer.
@@ -35,47 +37,59 @@
 % ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 % THE POSSIBILITY OF SUCH DAMAGE.
 
-function [output,srate,scale,chanlabels] = readNihonKodenM00(input)
+function [output, srate, scale, chanlabels] = readNihonKodenM00(input)
 
-% open the file
+
+%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Read the header. %%%
+%%%%%%%%%%%%%%%%%%%%%%%%
+% I found that using this legacy method is most straightforward for reading the header. (10/27/2024 Makoto)
 FID = fopen(input);
-
-% skip the first two lines
 firstLine  = fgetl(FID);
-secondLine = fgetl(FID);
-
-% Parse header info in the first line.
-C = strsplit(firstLine, ' ');
-if     size(C,1)==6 & size(C,2)==1
-    timePnts = str2num(C{1,1}(12:end));             % 12 letters are 'TimePoints='
-    numChans = str2num(C{2,1}(10:end));             % 10 letters are 'Channels='
-    srate    = round(1000/str2num(C{4,1}(22:end))); % 22 letters are 'SamplingInterval[ms]='
-    scale    = str2num(C{5,1}(9:end));              % 9  letters are 'Bins/uV='  
-    
-elseif size(C,1)==1 & size(C,2)==6 % New pattern confirmed in 2016. Depends on which strsplit() is used.
-    timePnts = str2num(C{1,1}(12:end));             % 12 letters are 'TimePoints='
-    numChans = str2num(C{1,2}(10:end));             % 10 letters are 'Channels='
-    srate    = round(1000/str2num(C{1,4}(22:end))); % 22 letters are 'SamplingInterval[ms]='
-    scale    = str2num(C{1,5}(9:end));              % 9  letters are 'Bins/uV='
-end
-
-% Parse header info in the second line.
-C = strsplit(secondLine, ' ');
-if ~isempty(str2double(C{2}))                       % Check to make sure the second line contains strings, not numbers.
-    chanlabels=C(2:end);                            % Line starts with a space, so labels will start in second cell.
-end
-
-% parse data structure
-tmpData = fscanf(FID, '%f');
 fclose(FID);
 
-% check consistency between header info and actual data size
-if length(chanlabels)>numChans
-    numChans = length(chanlabels);
-end
-if size(tmpData,1) ~= numChans*timePnts;
-    warning('Data size does not match header information. Check consistency.')
-end
+% Parse the header info.
+firstLineHeaderInfo  = strsplit(firstLine, ' ');
 
-% reshape the data for finalizing
-output = reshape(tmpData, [numChans timePnts]);
+timePointsIdx       = find(contains(firstLineHeaderInfo, 'TimePoints='));
+channelsIdx         = find(contains(firstLineHeaderInfo, 'Channels='));
+samplingIntervalIdx = find(contains(firstLineHeaderInfo, 'SamplingInterval[ms]='));
+binsMicroVIdx       = find(contains(firstLineHeaderInfo, 'Bins/uV='));
+
+timePointsCells       = strsplit(firstLineHeaderInfo{timePointsIdx}, '=');
+channelsCells         = strsplit(firstLineHeaderInfo{channelsIdx}, '=');
+samplingIntervalCells = strsplit(firstLineHeaderInfo{samplingIntervalIdx}, '=');
+binsMicroVCells       = strsplit(firstLineHeaderInfo{binsMicroVIdx}, '=');
+
+numTimePoints = str2double(timePointsCells{2}); % counts.
+numChannels   = str2double(channelsCells{2}); % counts.
+srate         = 1000/str2double(samplingIntervalCells{2}); % Hz.
+scale         = str2double(binsMicroVCells{2}); % bin/microV.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Read the data array. %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Import the whole data as a table. readtable() was supported in Matlab 2013b.
+importedTable = readtable(input, 'FileType', 'text', 'Delimiter', ' ', 'MultipleDelimsAsOne', true, 'VariableNamingRule', 'preserve');
+
+% Identify data columns: Scalp electrodes and Trigger.
+headerCells   = importedTable.Properties.VariableNames;
+dataColumnIdx = find(contains(headerCells, '-') | contains(headerCells, 'Trigger'));
+
+% Extract the data part.
+dataTable = importedTable(:, dataColumnIdx);
+
+% Prepare outputs.
+output     = single(table2array(dataTable));
+chanlabels = dataTable.Properties.VariableNames;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Validate the dimension of the imported data. %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if size(output,1) == numTimePoints & size(output,2) == numChannels
+    disp('Imported successfully.')
+else
+    error('Data dimension mismatch occured. Contact Makto: mmiyakoshi@ucsd.edu')
+end
